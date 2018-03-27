7c7bedc921919eab9b4320669051afd0a471735e
Will Taylor <taylorw@udel.edu>
2018-03-27 01:12:34 -0400

    Added Useless Red Diamond

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ MODID='arenarealm'
 VERSION='0.0.1'
 JAR_PATH="${BUILD_DIR}${MODID}-${VERSION}.jar"
 SERVER_PATH="${AWS_DNS}:${SERVER_MOD_DIR}"
-echo $SERVER_PATH
 ./gradlew build
 scp -i ${KEYFILE} ${JAR_PATH} ec2-user@${SERVER_PATH}
 cp ${JAR_PATH} ${CLIENT_MOD_DIR}
