98f58f352678823ff81a5f578b210e22a9586af2
Will Taylor <taylorw@udel.edu>
2018-04-24 08:54:46 -0400

    Added Sanguine spawns

--- src/main/java/com/titosworld/gladius/entity/ModEntities.java
+++ src/main/java/com/titosworld/gladius/entity/ModEntities.java
@@ -1,15 +1,23 @@
 package com.titosworld.gladius.entity;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.network.internal.EntitySpawnHandler;
 import net.minecraftforge.fml.common.registry.EntityEntry;
 import net.minecraftforge.fml.common.registry.EntityEntryBuilder;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+import net.minecraftforge.registries.ForgeRegistry;
+
+import com.google.common.collect.Iterables;
 import com.titosworld.gladius.Reference;
 
 @ObjectHolder(Reference.MODID)
@@ -25,7 +33,7 @@ public class ModEntities {
 			final EntityEntry[] entries = {
 					createBuilder("sanguine")
 						.entity(EntitySanguine.class)
-						.tracker(64, 10, true)
+						.tracker(80, 3, true)
 						.egg(0xad0000, 0x0)
 						.build()
 			};
@@ -49,11 +57,6 @@ public class ModEntities {
 	}
 	
 	private static void addSpawns() {
-		EntityRegistry.addSpawn(EntitySanguine.class, 100, 0, 1, EnumCreatureType.MONSTER);
+		EntityRegistry.addSpawn(EntitySanguine.class, 20, 1, 1, EnumCreatureType.AMBIENT, Iterables.toArray(ForgeRegistries.BIOMES, Biome.class));
 	}
-	
-	// Could be useful for biome specific mobs later...
-	/*private static Biome[] getBiomes(final BiomeDictionary.Type type) {
-		return BiomeDictionary.getBiomes(type).toArray(new Biome[0]);
-	}*/
 }
